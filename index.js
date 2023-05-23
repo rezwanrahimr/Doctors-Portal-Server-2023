c6a361e3a6752df24bb6790e0fbb229c94007a59
rezwanrahimr <rezwanrahim69@gmail.com>
2023-05-24 01:23:36 +0600

    create booking api and emplement logic

--- index.js
+++ index.js
@@ -24,17 +24,53 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
+    // Appointment Options Collections
+    const appointmentOptionsCollections = client
+      .db("doctors-portal")
+      .collection("appointment-options");
+    // Bookings Collections
+    const bookingCollections = client
+      .db("doctors-portal")
+      .collection("bookings");
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    //   Get Appointment Options
+    app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
+      const quary = {};
+      const cursor = appointmentOptionsCollections.find(quary);
+      const result = await cursor.toArray();
+      const bookingQuery = { appointmentDate: date };
+      const alreadyBooking = await bookingCollections
+        .find(bookingQuery)
+        .toArray();
+      result.forEach((element) => {
+        const optionBooked = alreadyBooking.filter(
+          (option) => option.treatmentName === element.name
+        );
+        // ...........
+        const bookSlot = optionBooked.map((element) => element.slots);
+        const remaining = element.slots.filter(
+          (slot) => !bookSlot.includes(slot)
+        );
+        element.slots = remaining;
+      });
+      res.send(result);
+    });
+
+    // Post Booking
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollections.insertOne(booking);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
